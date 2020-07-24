eb0db34c4e2e81452a5e758b3d2c0fa2e5579dc3
Aidan Duff <aidanpatrickduff@gmail.com>
2020-07-24 19:34:31 +0100

    Added navbar, lifted state from counters to App.js

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="bade badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
